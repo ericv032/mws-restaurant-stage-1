461ea1fa132a7606dcb0e05dfc18762cc1ef28a2
ericv032 <ericvazquez032@gmail.com>
2018-10-09 21:38:39 -0400

    Updated sw script

--- app/sw.js
+++ app/sw.js
@@ -1,6 +1,14 @@
-var CACHE_NAME = 'restaurant—reviews-static-v3';
-var urlsToCache = [
-	'/index.html',	'/restaurant.html',
+let staticCache = 'restaurant—reviews-static-v3';
+let imagesCache = 'restaurant—reviews-static-v3';
+
+const allCaches = [
+	staticCache,
+	imagesCache
+];
+const urlsToCache = [
+				'/',
+				'/index.html',
+				'/restaurant.html',
 	      '/css/styles.css',
 	      '/scripts/dbhelper.js',
 				'/scripts/idb.js',
@@ -23,74 +31,144 @@ var urlsToCache = [
     ];
 
 self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
+	event.waitUntil(
+		caches.open(staticCache).then(function(cache) {
+			console.log('Cache opened');
+			return cache.addAll(urlsToCache);
+		}).catch(error => console.log(`Open cache failed: ${error}`))
+	);
 });
 
+self.addEventListener('activate', function (event) {
+	console.log('Service Worker activated');
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        // return fetch(event.request);
-      }
-    )
-  );
+	event.waitUntil(
+		caches.keys().then(function (cacheNames) {
+			return Promise.all(
+				cacheNames.filter(function (thisCacheName) {
+					return thisCacheName.startsWith('restaurant-') && !allCaches.includes(thisCacheName);
+				}).map(function (thisCacheName) {
+					return caches.delete(thisCacheName);
+				})
+			);
+		})
+	);
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        var fetchRequest = event.request.clone();
+self.addEventListener('fetch', (event) => {
+	const requestUrl = new URL(event.request.url);
+	// console.log(`Log request url: ${requestUrl.pathname}`);
+	if (requestUrl.pathname.startsWith('/img/')) {
+		// console.log(`Log img request url: ${requestUrl.pathname.startsWith('/img/')}`);
+		event.respondWith(serveImage(event.request));
+		return;
+	}
 
-        return fetch(fetchRequest).then(
-          function(response) {
-            // Check if we received a valid response
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-            var responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then(function(cache) {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          }
-        );
-      })
-    );
+	event.respondWith(
+		caches.match(event.request).then(response => {
+			if (response) {
+				console.log(`Service worker found in cache: ${event.request.url}`);
+				return response;
+			}
+			return fetch(event.request).then(networkResponse => {
+				if (networkResponse.status === 404) {
+					// console.log(networkResponse.status);
+					return;
+				}
+				return caches.open(staticCache).then(cache => {
+					cache.put(event.request.url, networkResponse.clone());
+					return networkResponse;
+				})
+			})
+		}).catch(error => {
+			console.log('Error:', error);
+			return;
+		})
+	);
 });
 
-self.addEventListener('activate', function(event) {
+serveImage = (request) => {
 
-  var cacheWhitelist = ['restaurant—reviews-static-v3'];
+	// Check if images are in cache if not then store them
+	return caches.open(imagesCache).then(function (cache) {
+		return cache.match(request.url).then(function (response) {
+			if (response) return response;
 
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
+			return fetch(request).then(function (networkResponse) {
+				cache.put(request.url, networkResponse.clone());
+				return networkResponse;
+			});
+		});
+	});
+}
+
+self.addEventListener('message', (event) => {
+    console.log(event);
+
+    // var messages = JSON.parse(event.data);
+    if (event.data.action === 'skipWaiting') {
+       self.skipWaiting();
+    }
+});
+
+self.addEventListener('sync', function (event) {
+	if (event.tag == 'myFirstSync') {
+		const DBOpenRequest = indexedDB.open('restaurants', 1);
+		DBOpenRequest.onsuccess = function (e) {
+			db = DBOpenRequest.result;
+			let tx = db.transaction('offline-reviews', 'readwrite');
+			let store = tx.objectStore('offline-reviews');
+			// 1. Get submitted reviews while offline
+			let request = store.getAll();
+			request.onsuccess = function () {
+				// 2. POST offline reviews to network
+				for (let i = 0; i < request.result.length; i++) {
+					fetch(`http://localhost:1337/reviews/`, {
+						body: JSON.stringify(request.result[i]),
+						cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+						credentials: 'same-origin', // include, same-origin, *omit
+						headers: {
+							'content-type': 'application/json'
+						},
+						method: 'POST',
+						mode: 'cors',
+						redirect: 'follow',
+						referrer: 'no-referrer', 
+					})
+					.then(response => {
+						return response.json();
+					})
+					.then(data => {
+						let tx = db.transaction('all-reviews', 'readwrite');
+						let store = tx.objectStore('all-reviews');
+						let request = store.add(data);
+						request.onsuccess = function (data) {
+							//TODO: add data (= one review) to view
+							let tx = db.transaction('offline-reviews', 'readwrite');
+							let store = tx.objectStore('offline-reviews');
+							let request = store.clear();
+							request.onsuccess = function () {
+								console.log('this runs but empty');
+							 };
+							request.onerror = function (error) {
+								console.log('Unable to clear offline-reviews objectStore', error);
+							}
+						};
+						request.onerror = function (error) {
+							console.log('Unable to add objectStore to IDB', error);
+						}
+					})
+					.catch(error => {
+						console.log('Unable to make a POST fetch', error);
+					})
+				}
+			}
+			request.onerror = function (e) {
+				console.log(e);
+			}
+		}
+		DBOpenRequest.onerror = function (e) {
+			console.log(e);
+		}
+	}
 });
